a300a7b621f0b72361f11458169d9117b7502e2d
Vali Tugui <vali.tuguii@gmail.com>
2021-12-19 19:34:21 +0000

    Some addition to my third day

--- src/test/java/subject01/Test03.java
+++ src/test/java/subject01/Test03.java
@@ -3,6 +3,6 @@ package subject01;
 public class Test03 {
 
     public static void main(String[] args) {
-        System.out.println("I did my third day change");
+        System.out.println("I did my third day changes to my project");
     }
 }
