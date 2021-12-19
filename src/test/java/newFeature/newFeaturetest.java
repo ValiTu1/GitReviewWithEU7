5ca8d0ca9907d033963ab4f87a6169f42a2dbaf7
Vali Tugui <vali.tuguii@gmail.com>
2021-12-19 19:38:02 +0000

    New trials

---
+++ src/test/java/newFeature/newFeaturetest.java
@@ -0,0 +1,9 @@
+package newFeature;
+
+public class newFeaturetest {
+
+    public static void main(String[] args) {
+
+        System.out.println("I'm making some trial changes to my project");
+    }
+}
